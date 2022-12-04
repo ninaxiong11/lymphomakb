d34ff4ea65dc2b808326c630bfbfea59026c22c9
ninasxiong <ninaxiong11@gmail.com>
2022-12-04 14:54:45 -0500

    add scripts

---
+++ types.py
@@ -0,0 +1,73 @@
+from pyorient.ogm import Graph, Config
+from pyorient.ogm.declarative import declarative_node, declarative_relationship
+from pyorient.ogm.property import String, Integer
+
+def initialize_graph_schema(graph):
+    
+    Node = declarative_node()
+    Relationship = declarative_relationship()
+
+    class Variant(Node):
+        element_type = "variant"
+        element_plural = "variants"
+        name = String()
+        source = String()
+
+    class SNV(Variant):
+        element_type = "snv"
+        element_plural = "snvs"
+        build = String()
+        gene = String()
+        transcript = String()
+        genomic_pos = Integer()
+        coding_pos = Integer()
+        ref_nt = String()
+        alt_nt = String()
+        protein_pos = Integer()
+        ref_aa = String()
+        alt_aa = String()
+
+    class CopyGain(Variant):
+        element_type = "copygain"
+        element_plural = "copygains"
+        build = String()
+        gene = String()
+        copies = Integer()
+        genomic_loc = String()
+        cytogenetic_loc = String()
+
+    class Statement(Node):
+        element_type = "statement"
+        element_plural = "statements"
+        source = String()
+        significance = String()
+
+    class Disease(Node):
+        element_type = "disease"
+        element_plural = "diseases"
+        name = String(nullable=False)
+
+    class Drug(Node):
+        element_type = "drug"
+        element_plural = "drugs"
+        name = String(nullable=False, unique=True)
+
+    class Involves(Relationship):
+        label = "involves"
+
+    # class Property(Node):
+    #     pass
+
+    # class HasProperty(Relationship):
+    #    pass
+
+    # class SubclassOf(Relationship):
+    #     pass
+
+    # class Infers(Relationship):
+    #     pass
+
+    graph.create_all(Node.registry)
+    graph.create_all(Relationship.registry)
+
+    return graph
